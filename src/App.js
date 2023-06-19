1360cf60eae46b9130a6e07dfa835ef0a8c01ffa
thiagotellini <thiagotellini@gmail.com>
2023-06-19 12:14:42 -0300

    Acrescentando consutal com enter

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import { useState } from 'react';
-import {FiSearch} from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import InputMask from 'react-input-mask';
 import './styles.css';
 import api from './services/api';
-import InputMask from 'react-input-mask';
 
 function App() {
-
   const [input, setInput] = useState('');
   const [cep, setCep] = useState({});
 
-  async function handleSearch(){
-    //01310930/jason/
-
-    if(input === ''){
-      alert("Preencha o CEP!")
+  async function handleSearch() {
+    if (input === '') {
+      alert('Preencha o CEP!');
       return;
     }
 
-    // Remove non-numeric characters from the input
     const numericInput = input.replace(/\D/g, '');
 
-    try{
-      const response = await api.get(`${input}/json`);
-      setCep(response.data)
-      setInput("")
+    try {
+      const response = await api.get(`${numericInput}/json`);
+      setCep(response.data);
+      setInput('');
+    } catch {
+      alert('CEP INVÁLIDO!');
+      setInput('');
+    }
+  }
 
-    }catch{
-      alert("CEP INVÁLIDO!");
-      setInput("")
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      handleSearch();
     }
   }
 
@@ -36,32 +37,31 @@ function App() {
       <h1 className="title">Buscador de CEP</h1>
 
       <div className="containerInput">
-      <InputMask
-        mask="99999-999"
-        maskChar=""
-        type="text"
-        placeholder="Digite o CEP..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
+        <InputMask
+          mask="99999-999"
+          maskChar=""
+          type="text"
+          placeholder="Digite o CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
 
         <button className="buttonSearch" onClick={handleSearch}>
-          <FiSearch size={25} color='#FFF'/>
+          <FiSearch size={25} color="#FFF" />
         </button>
       </div>
 
       {Object.keys(cep).length > 0 && (
-         <main className='main'>
+        <main className="main">
           <h2>CEP: {cep.cep}</h2>
-  
           <span>{cep.logradouro}</span>
           <span>Bairro: {cep.bairro}</span>
-          <span>{cep.localidade} - {cep.uf}</span>
-           
-       </main>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
       )}
-     
-
     </div>
   );
 }
